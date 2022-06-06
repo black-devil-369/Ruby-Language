7472d1214d65eb32f4be9e5546ad97f612f6ce13
black-devil-369 <ajaychauhan1262@gmail.com>
2022-06-05 22:28:05 -0400

    coding

---
+++ 3.rb
@@ -0,0 +1,14 @@
+class Pattern
+    def code
+        puts"Enter the Number for printing star"
+        n = gets.chomp.to_i
+        for i in (1..n)
+            for j in(1..i)
+                print" * "
+            end
+             puts" "
+        end
+    end
+end
+obj1 = Pattern.new()
+obj1.code
